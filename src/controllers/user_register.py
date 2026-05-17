4f23490d3eeff1540d254d5777837bf947f5cbe8
João Gabriel <joaogabrielf124@gmail.com>
2026-05-17 11:13:13 -0300

    feat: Implementing UserRegister and UserFinder (need tests)

--- src/controllers/user_register.py
+++ src/controllers/user_register.py
@@ -2,4 +2,29 @@ from src.models.repositories.interfaces.users_repository import UsersRepositoryI
 
 class UserRegister:
     def __init__(self, users_repository: UsersRepositoryInterface) -> None:
-        self.users_repository = users_repository
+        self.__users_repository = users_repository
+
+    async def register_user(self, user_data: dict) -> dict:
+        self.__validate_user_data(user_data)
+        await self.__registry_user(user_data)
+        return self.__format__response(user_data)
+
+    def __validate_user_data(self, user_data: dict) -> None:
+        age = user_data["age"]
+        uf = user_data["uf"].upper()
+
+        if uf not in ["MG", "BA", "CE", "SC", "MT"]:\
+            raise Exception("Estado Invalido para cadastro")
+        
+        if age < 0 or age > 120:
+            raise Exception("Idade Invalida para Cadastro")
+        
+    async def __registry_user(self, user_data: dict) -> None:
+        self.__users_repository.insert_users(user_data)
+
+    def __format__response(self, user_data: dict) -> dict:
+        return {
+            "type": "USERS",
+            "count": 1,
+            "attributes": user_data
+        }
