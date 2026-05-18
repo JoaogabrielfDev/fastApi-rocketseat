c7296eebf58771e818c36681a5452a4ca58053b9
João Gabriel <joaogabrielf124@gmail.com>
2026-05-18 13:49:22 -0300

    feat: implementing criar_usuarios and buscar_usuarios_por_nome routes with composer and validators

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "src.main.server.server:app",
-        host='0.0.0.0',
+        host='localhost',
         port=3001,
         reload=True
     )
--- src/main/routes/users_routes.py
+++ src/main/routes/users_routes.py
@@ -1,13 +1,35 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from src.views.http_types.http_request import HttpResquest
+from src.validators.user_register_validator import UserInput
+
+from src.main.composer.user_finder_composer import user_finder_composer
+from src.main.composer.user_register_composer import user_register_composer
+
 
 users_routes = APIRouter(tags=["Usuarios"])
 
 @users_routes.post("/users")
-async def criar_usuario():
+async def criar_usuario(body: UserInput):
+    http_request = HttpResquest(body=dict(body))
+    user_register = user_register_composer()
+
+    http_response = await user_register.handle_register_user(http_request)
 
     return JSONResponse(
-        content={"Ola": "Mundo"},
-        status_code=200
+        content=http_response.body,
+        status_code=http_response.status_code
     )
 
+
+@users_routes.get("/users/{user_name}")
+async def buscar_usuarios_por_nome(user_name: str):
+    http_request = HttpResquest(path_params={"user_name": user_name})
+    user_finder = user_finder_composer()
+
+    http_response = await user_finder.handle_find_user_by_name(http_request)
+
+    return JSONResponse(
+        content=http_response.body,
+        status_code=http_response.status_code
+    )
